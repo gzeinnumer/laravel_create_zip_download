fa3a13942aae9602cf8e15fd403aee6820f31f52
M. Fadli Zein <45892408+gzeinnumer@users.noreply.github.com>
2022-07-25 02:32:31 +0700

    commit

--- README.md
+++ README.md
@@ -1,2 +1,9 @@
 # laravel_create_zip_download
- 
+
+[Source](https://www.nicesnippets.com/blog/how-to-create-zip-file-and-download-in-laravel-8) 
+
+Make Folder 
+
+```
+public/myFiles/test
+```
--- app/Http/Controllers/ZipController.php
+++ app/Http/Controllers/ZipController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-use File;
+use Illuminate\Support\Facades\File;
 use ZipArchive;
 
 class ZipController extends Controller
@@ -13,23 +13,66 @@ class ZipController extends Controller
      *
      * @return \Illuminate\Http\Response
      */
+    // public function downloadZip()
+    // {
+    //     $zip = new ZipArchive;
+
+    //     $fileName = 'myNewFile.zip';
+
+    //     if ($zip->open(public_path($fileName), ZipArchive::CREATE) === TRUE) {
+    //         $files = File::files(public_path('myFiles'));
+
+    //         foreach ($files as $key => $value) {
+    //             $relativeNameInZipFile = basename($value);
+    //             $zip->addFile($value, $relativeNameInZipFile);
+    //         }
+
+    //         $zip->close();
+    //     }
+
+    //     return response()->download(public_path($fileName));
+    // }
+
     public function downloadZip()
     {
-        $zip = new ZipArchive;
-
         $fileName = 'myNewFile.zip';
 
-        if ($zip->open(public_path($fileName), ZipArchive::CREATE) === TRUE) {
-            $files = File::files(public_path('myFiles'));
+        $filePath = public_path($fileName);
+        $zip = new \ZipArchive();
 
-            foreach ($files as $key => $value) {
-                $relativeNameInZipFile = basename($value);
-                $zip->addFile($value, $relativeNameInZipFile);
-            }
-
-            $zip->close();
+        if ($zip->open($filePath, \ZipArchive::CREATE) !== true) {
+            throw new \RuntimeException('Cannot open ' . $filePath);
         }
 
-        return response()->download(public_path($fileName));
+        $this->addContent($zip, public_path('myFiles'));
+        $zip->close();
+    }
+
+    private function addContent(\ZipArchive $zip, string $path)
+    {
+        /** @var SplFileInfo[] $files */
+        $iterator = new \RecursiveIteratorIterator(
+            new \RecursiveDirectoryIterator(
+                $path,
+                \FilesystemIterator::FOLLOW_SYMLINKS
+            ),
+            \RecursiveIteratorIterator::SELF_FIRST
+        );
+
+        while ($iterator->valid()) {
+            if (!$iterator->isDot()) {
+                $filePath = $iterator->getPathName();
+                $relativePath = substr($filePath, strlen($path) + 1);
+
+                if (!$iterator->isDir()) {
+                    $zip->addFile($filePath, $relativePath);
+                } else {
+                    if ($relativePath !== false) {
+                        $zip->addEmptyDir($relativePath);
+                    }
+                }
+            }
+            $iterator->next();
+        }
     }
 }
